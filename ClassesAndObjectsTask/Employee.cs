c564150c2999c2245505d77cc17044d7155cf057
sandro <wrek.yamom@gmail.com>
2024-12-19 23:00:53 +0400

    ClassesAndObjects

---
+++ ClassesAndObjectsTask/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjectsTask
+{
+    public class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee() 
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public void ChangeSurname(string updateSurname)
+        {
+            this.surname = updateSurname;
+        }
+
+        public string EmployeeInfo() => $"Surname: {this.surname}, Age: {this.age}";
+
+        private string GetAge() => this.age.ToString();
+    }
+}
